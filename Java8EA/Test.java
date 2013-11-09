91c784854d04beb441091b88f62b7a25048da512
koba-e964 <silentkiddie-2012@yahoo.co.jp>
2013-11-09 22:11:29 +0900

    Test modification

--- Java8EA/Test.java
+++ Java8EA/Test.java
@@ -1,4 +1,5 @@
 import java.util.function.*;
+import java.util.stream.*;
 
 public class Test{
 	public static void main(String[] args){
@@ -10,7 +11,11 @@ public class Test{
 		ic.accept(3);
 		System.out.println(op.applyAsInt(4));
 		System.out.println(adder(3).applyAsInt(10));
-		return;
+		Sequence seq=new Sequence(x->2*x[0],new int[]{1});
+		IntStream is=seq.ints(10);
+		is.forEach(v->{
+			System.out.println(v);
+		});
 	}
 	static IntUnaryOperator adder(int n){
 		return t->t+n+0xdead;
