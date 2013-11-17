dac49b6cad995b2a31740e5f4c59cecb1705fc43
koba-e964 <silentkiddie-2012@yahoo.co.jp>
2013-11-17 20:40:11 +0900

    Java8EA/Test.java

--- Java8EA/Test.java
+++ Java8EA/Test.java
@@ -2,6 +2,7 @@ import java.util.function.*;
 import java.util.stream.*;
 import kobae964_lib.Sequence;
 
+
 public class Test{
 	public static void main(String[] args){
 		int n=4;
@@ -13,10 +14,16 @@ public class Test{
 		System.out.println(op.applyAsInt(4));
 		System.out.println(adder(3).applyAsInt(10));
 		Sequence seq=new Sequence(x->2*x[0],new int[]{1});
-		IntStream is=seq.ints(10);
-		is.forEach(v->{
-			System.out.println(v);
-		});
+		IntStream is=seq.ints(3);
+		is.forEach(System.out::println);
+		ToIntBiFunction<Integer,String> fisi=(Integer x,String y)->{
+		int z=x+y.length();
+		return z;
+		};
+		IntUnaryOperator fii=(int x)->x;
+		System.out.println(fisi.applyAsInt(3,"test"));
+		((IntUnaryOperator)x->x*x).applyAsInt(20131124);
+		//System.out.println((t->t*t).toString());
 	}
 	static IntUnaryOperator adder(int n){
 		return t->t+n+0xdead;
