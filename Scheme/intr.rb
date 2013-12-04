c49c4d037d1de301e2be9d33e72d4d96cff7b13b
koba-e964 <silentkiddie-2012@yahoo.co.jp>
2013-12-04 17:08:10 +0900

    intr.rb
    intr.rb reads lib.txt
    intr.rb became less noisy

--- Scheme/intr.rb
+++ Scheme/intr.rb
@@ -11,6 +11,32 @@ global_map=SymMap.new()
 RbScmEval::set_global_map(global_map)
 RbScmEval::add_initial_operator(global_map)
 
+def run(str)
+	tokens=tokenize(str)
+	pos=0
+	res=nil
+	while(pos<tokens.size)
+		#p [pos,tokens.size,tokens[pos...tokens.size]]
+		sobj,len=parse_expr(tokens[pos...tokens.size])
+		res=sobj_eval(sobj)
+		pos+=len
+	end
+	return res
+end
+
+
+def read_file(filename)
+	str=''
+	fp=open(filename,'r')
+	for line in fp
+		str+=line
+	end
+	fp.close
+	run(str)
+	nil
+end
+read_file('lib.txt')
+
 
 while true
 	print ">>>"
@@ -19,10 +45,13 @@ while true
 	if(line=="exit")
 		break
 	end
+	if(line=="env")
+		p global_map
+		next
+	end
 	begin
 		sexpr,slen=parse_expr(tokenize(line))
 		puts sobj_eval_sym(sexpr,global_map)
-		p global_map
 	rescue => ex
 		p ex
 		puts ex.backtrace
