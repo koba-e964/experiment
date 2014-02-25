822d420be2b6fca9344522db6cae227e5e48230d
koba-e964 <silentkiddie-2012@yahoo.co.jp>
2014-02-25 17:36:38 +0900

    [refactor] rewrite tests(in progress)
    rewrite tests (using test_eval, assert_equal)
    add tests for named let

--- Scheme/test-eval.rb
+++ Scheme/test-eval.rb
@@ -16,10 +16,16 @@ def assert_equal(actual,exp)
 end
 
 def test_eval(expr,expected)
-	actual=run(expr)
 	exp_s=ruby_to_SObj(expected)
-	if actual != exp_s
-		puts "expr:"+expr+", expected:"+exp_s.to_s+", actual:"+actual.to_s
+	begin
+			actual=run(expr)
+		if actual != exp_s
+			puts "expr:"+expr+", expected:"+exp_s.to_s+", actual:"+actual.to_s
+		end
+		return
+	rescue => ex
+		puts "expr:"+expr+", expected:"+exp_s.to_s+", got:"
+		p ex
 	end
 end
 
@@ -118,8 +124,9 @@ EOS
 
 
 puts "----- 6.1 Equivalence predicates:"
-assert_equal(run('(eqv? (quote a) (quote a))'), ruby_to_SObj(true))
-assert_equal(run('(eqv? (list 1) (list 1))'), ruby_to_SObj(false))
+test_eval '(eqv? (quote a) (quote a))', true
+test_eval '(eqv? (list 1) (list 1))', false
+test_eval '(begin (define x (list 2 1 0)) (list (eqv? x x) (eqv? x (list 2 1 0))))', [true,false]
 
 puts "----- 6.2.4 numerical constants:"
 test_eval '(quote (#b1010 #o777 #d10000 #xffff))', [10,0777,10000,0xffff]
@@ -183,11 +190,24 @@ test_eval '(assoc-general = 10 (quote ((1 4) (10 3) (2 10))))',[10,3]
 
 
 
-puts "-----do:"
+puts "----- 4.2.4 Iteration:"
+# do
 test_eval '(do ((s ()) (i 0 (+ i 1))) ((= i 5) s) (set! s (cons i s)))',[4,3,2,1,0]
 
-puts "-----eqv?"
-test_eval '(begin (define x (list 2 1 0)) (list (eqv? x x) (eqv? x (list 2 1 0))))',[true,false]
+# named let
+expr=<<EOS
+(let loop ((numbers '(3 -2 1 6 -5))
+		(nonneg '())
+		(neg '()))
+	(cond ((null? numbers) (list nonneg neg))
+		((>= (car numbers) 0)
+			(loop (cdr numbers) (cons (car numbers) nonneg) neg))
+		((< (car numbers) 0)
+			(loop (cdr numbers) nonneg (cons (car numbers) neg)))))
+EOS
+test_eval expr, [[6,1,3],[-5,-2]]
+
+
 
 puts "-----case"
 test_eval '(case (+ 10 5) ((1 10 15) (quote first)) ((2 13 16) (quote second)) (else (quote else)))', make_symbol("first")
