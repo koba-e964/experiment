89c12ec11838472f8d7b21051e13bfe37ef4f1ee
koba-e964 <silentkiddie-2012@yahoo.co.jp>
2014-04-07 11:29:01 +0900

    rewrite parser (HaskCalc.hs)
    change functions to monadic ones
    cleanup

--- haskcalc/HaskCalc.hs
+++ haskcalc/HaskCalc.hs
@@ -1,66 +1,66 @@
 module HaskCalc where
 import Control.Applicative ((<$>), (*>), (<*), some)
+import Control.Monad (liftM)
 import Control.Monad.Trans.State.Strict ()
 import Data.List (foldl')
 import Data.Map (Map, (!), keys, fromList)
-import Text.ParserCombinators.Parsec ((<|>), Parser, char, digit, getInput, parse, pzero, setInput, string, try)
+import Text.Parsec.Prim (ParsecT, parserZero, runParsecT, try, runPT)
+import Text.ParserCombinators.Parsec ((<|>), Parser, char, digit, getInput, parse, pzero, setInput, string)
 
-number:: Parser Double
+type MParser m a= ParsecT String () m a
+
+number :: Monad m => MParser m Double
 number=do
-	x<-some (digit <|> char '.' <|> char 'e' <|> char '-')
-	let{pair=reads x;}
+	x <- some (digit <|> char '.' <|> char 'e' <|> char '-')
+	let{pair = reads x;}
 	case pair of
-		[]->pzero;
-		[(x,rest)]->do
-			gi<-getInput
-			setInput (rest++gi)
+		[] -> parserZero;
+		[(x, rest)]->do
+			gi <- getInput
+			setInput $ rest ++ gi
 			return x;
 
-type DoubleBiop=Double->Double->Double
+type DoubleBiop = Double -> Double -> Double
 -- deriveOne returns a derivation tree (of type (Double,[(a,Double)])).
-deriveOne :: Map String a->Parser Double->Parser (Double,[(a,Double)]) -- a:Operator type
-deriveOne mp parent=do
-	let{ops=keys mp;}
-	x<-parent
-	(foldl' (<|>) pzero [try(do{
+deriveOne :: Monad m => Map String a -> MParser m Double -> MParser m (Double, [(a, Double)]) -- a:Operator type
+deriveOne mp parent = do
+	let {ops = keys mp;}
+	x <- parent
+	(foldl' (<|>) parserZero [try $ do{
 		string op;
-		(t,y)<-deriveOne mp parent;
-		return $ (x,(mp!op,t):y)
-	})|op<-ops])<|>return (x,[])
+		(t, y) <- deriveOne mp parent;
+		return $ (x, (mp ! op, t) : y)
+	} | op <- ops]) <|> return (x, [])
 
 simplify :: (Double,[(DoubleBiop,Double)])->Double
-simplify derivTree=let (x,ls)=derivTree in
-	foldl (\x (op,y)->op x y) x ls
+simplify derivTree = let (x,ls) = derivTree in
+	foldl (\x (op, y) -> op x y) x ls
 
-deriveOneAndSimplify::Map String DoubleBiop->Parser Double->Parser Double
-deriveOneAndSimplify mp parent=simplify <$> (deriveOne mp parent)
+deriveOneAndSimplify :: Monad m => Map String DoubleBiop -> MParser m Double -> MParser m Double
+deriveOneAndSimplify mp parent = liftM simplify $ deriveOne mp parent
 
-exponential ::Parser Double
-exponential = deriveOneAndSimplify (fromList [("^",(**))]) (number<|> paren_expr)
+exponential :: Monad m => MParser m Double
+exponential = deriveOneAndSimplify (fromList [("^",(**))]) (number <|> paren_expr)
 
-data MultiDiv=Multi|Div|Rem deriving (Eq,Show,Ord)
 
 doubleRem x 0=x
-doubleRem x y=let i=fromIntegral (floor (x/y)) in x-y*i
+doubleRem x y = let i = fromIntegral $ floor $ x / y in x - y * i
 
-multi_list :: Parser (Double,[(MultiDiv,Double)])
-multi_list = deriveOne (fromList [("*",Multi),("/",Div),("%",Rem)]::Map String MultiDiv) exponential
-multi :: Parser Double
-multi=deriveOneAndSimplify (fromList [("*",(*)),("/",(/)),("%",doubleRem)]) exponential
+multi :: Monad m => MParser m Double
+multi = deriveOneAndSimplify (fromList [("*",(*)), ("/",(/)), ("%",doubleRem)]) exponential
 
-data AddSubt=Add|Subt deriving (Eq,Show,Ord)
-additive_list :: Parser (Double,[(AddSubt,Double)])
-additive_list = deriveOne (fromList [("+",Add),("-",Subt)]::Map String AddSubt) multi
-additive :: Parser Double
-additive=deriveOneAndSimplify (fromList [("+",(+)),("-",(-))]) multi
+additive :: Monad m => MParser m Double
+additive = deriveOneAndSimplify (fromList [("+",(+)), ("-",(-))]) multi
 
-paren_expr :: Parser Double
-paren_expr=char '(' *> expr <* char ')'
+paren_expr :: Monad m => MParser m Double
+paren_expr = char '(' *> expr <* char ')'
 
-expr :: Parser Double
-expr=additive <|> paren_expr
+expr :: Monad m => MParser m Double
+expr = additive <|> paren_expr
 
-eval :: String->Either String Double
-eval str=case parse expr "Parse error" str of
-	Left x->Left (show x)
-	Right v->Right v
+eval :: Monad m => String -> m (Either String Double)
+eval str = do
+  res <- runPT expr () "Parse error" str
+  return $ case res of
+    Left x  -> Left (show x)
+    Right v -> Right v
