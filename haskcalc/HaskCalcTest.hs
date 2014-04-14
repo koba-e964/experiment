bd63845d7f04637e87cc0e984994c9042b27399a
koba-e964 <silentkiddie-2013@yahoo.co.jp>
2014-04-14 16:14:28 +0900

    fix HaskCalcTest.hs
    tests work now

--- haskcalc/HaskCalcTest.hs
+++ haskcalc/HaskCalcTest.hs
@@ -1,7 +1,12 @@
+import Control.Monad.Identity (Identity, runIdentity)
 import Test.HUnit
 import HaskCalc
 main :: IO ()
 main=runTestTT alltests >> return ()
+
+-- eval with m = Identity
+eval0 :: String -> Either String Double
+eval0 = runIdentity . eval 
 
 -- utility for Double-value check
 assertDoubleEqual :: String->Double->Double->Double->Assertion
@@ -17,7 +22,7 @@ expected ~===? actual = TestCase (assertDoubleEqual ("expected:"++(show expected
 actual ~?=== expected = TestCase (assertDoubleEqual ("expected:"++(show expected)++", actual:"++(show actual))
 	expected actual 1e-11)
 
--- tests for eval
+-- tests for eval0
 
 alltests=TestList [tests_muladd, tests_rem, tests_exp, tests_paren]
 -- tests for +-*/
@@ -26,27 +31,27 @@ isLeft (Left _)=True
 isLeft _=False
 
 tests_muladd=TestList [
-	Right 1 ~=? eval "1",
-	Right 5 ~=? eval "1+4",
-	Right 9 ~=? eval "1+2*4",
-	TestCase $ assertBool "error" (isLeft (eval "-+*")),
-	eval "1+2*3+4" ~?= Right 11]
+	Right 1 ~=? eval0 "1",
+	Right 5 ~=? eval0 "1+4",
+	Right 9 ~=? eval0 "1+2*4",
+	TestCase $ assertBool "error" (isLeft (eval0 "-+*")),
+	eval0 "1+2*3+4" ~?= Right 11]
 -- tests for % (remainder)
 tests_rem=TestList [
-	eval "6%4" ~?= Right 2,
-	(case eval "10.43%2.5" of Right x->x) ~?=== 0.43]
+	eval0 "6%4" ~?= Right 2,
+	(case eval0 "10.43%2.5" of Right x->x) ~?=== 0.43]
 
 -- tests for ^ (exponential)
 tests_exp=TestList [
-	eval "4^4" ~?= Right 256,
-	eval "0.0^0.0" ~?= Right 1,
-	eval "0.0^-1.0" ~?= Right (1/0),
-	eval "2*3^4" ~?= Right 162]
+	eval0 "4^4" ~?= Right 256,
+	eval0 "0.0^0.0" ~?= Right 1,
+	eval0 "0.0^-1.0" ~?= Right (1/0),
+	eval0 "2*3^4" ~?= Right 162]
 
 -- tests for ()
 tests_paren=TestList [
-	eval "3+2*(5-9)" ~?= Right (-5),
-	eval "(3.5*4)-11" ~?= Right 3]
+	eval0 "3+2*(5-9)" ~?= Right (-5),
+	eval0 "(3.5*4)-11" ~?= Right 3]
 
 
 
